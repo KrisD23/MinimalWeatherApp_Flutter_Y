22376f33f93f18e00c1a08cc1f024ace057e90b7
Krishan Dwivedi <krishandwive9@gmail.com>
2023-10-19 13:01:15 +0530

    App completed with animation

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -29,6 +29,31 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  // weather animation
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -48,10 +73,12 @@ class _WeatherPageState extends State<WeatherPage> {
             Text(_weather?.cityName ?? "loading city name..."),
 
             // animation
-            Lottie.asset('assets/thunder.json'),
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
 
             // temperature
             Text('${_weather?.temperature.round()}°C'),
+            // weather condition
+            Text(_weather?.mainCondition ?? ""),
           ],
         ),
       ),
